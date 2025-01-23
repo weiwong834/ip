7b5dc1db56bedd87308934a2e7b62f0601e63524
weiwong834 <weiyin111604@gmail.com>
2025-01-23 23:06:57 +0800

    echo

--- src/main/java/Squid.java
+++ src/main/java/Squid.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Squid {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String line = "____________________________________________________________\n";
         String greeting = " Hello! I'm Squid\n"
                         + " What can I do for you?\n";
         String goodbye = " Bye. Hope to see you again soon!\n";
-        System.out.println(line + greeting + line + goodbye + line);
+        System.out.println(line + greeting + line);
+        while (true) {
+            if (!sc.hasNextLine()) break;
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(line + goodbye + line);
+                break;
+            } else {
+                System.out.println(line + input + "\n" + line);
+            }
+        }
+        sc.close();
     }
 }
