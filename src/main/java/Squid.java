6c09b75cf96385e0ea92f13e430da7640ed3ba97
weiwong834 <weiyin111604@gmail.com>
2025-01-23 22:32:46 +0800

    Renamed Duke to Squid, added greeting + goodbye

---
+++ src/main/java/Squid.java
@@ -0,0 +1,9 @@
+public class Squid {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________\n";
+        String greeting = " Hello! I'm Squid\n"
+                        + " What can I do for you?\n";
+        String goodbye = " Bye. Hope to see you again soon!\n";
+        System.out.println(line + greeting + line + goodbye + line);
+    }
+}
