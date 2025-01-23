2518ba8c5f1214db57ea935d7ab5654c4ab55cca
weiwong834 <weiyin111604@gmail.com>
2025-01-23 23:22:22 +0800

    Add, list

--- src/main/java/Squid.java
+++ src/main/java/Squid.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Squid {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
 
         String line = "____________________________________________________________\n";
         String greeting = " Hello! I'm Squid\n"
@@ -16,8 +18,15 @@ public class Squid {
             if (input.equals("bye")) {
                 System.out.println(line + goodbye + line);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i) + "\n");
+                }
+                System.out.println(line);
             } else {
-                System.out.println(line + input + "\n" + line);
+                tasks.add(input);
+                System.out.println(line + "added: " + input + "\n" + line);
             }
         }
         sc.close();
