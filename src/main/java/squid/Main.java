ac34c30a4a48298c06471770e155192f53080217
weiwong834 <weiyin111604@gmail.com>
2025-02-16 17:38:42 +0800

    Add GUI configuration

---
+++ src/main/java/squid/Main.java
@@ -0,0 +1,31 @@
+package squid;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Squid using FXML.
+ */
+public class Main extends Application {
+
+    private Squid squid = new Squid("data/squid.txt");
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setSquid(squid);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
